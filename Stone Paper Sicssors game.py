4f1451be50c9b5bf536ff26bd719bed8373de4f0
Bishal Mondal <89398209+BishalMondal07@users.noreply.github.com>
2023-03-08 09:55:59 +0530

    Add files via upload

---
+++ Stone Paper Sicssors game.py
@@ -0,0 +1,35 @@
+import random
+objects = ["Stone", "Paper", "Scissor"]
+Comp = random.choice(objects)
+
+S = ("Stone")
+P = ("Paper")
+Ss = ("Scissor")
+
+print("Computer turn : S(for stone), P(for paper), Ss(for sicssor)")
+b = input("Player 1 turn : Type S(for Stone), P(for Paper), Ss(Sicssor) :")
+
+print("Computer played" , Comp)
+
+if (Comp == S and b == "S") :
+    print("It's a tie game !")
+elif (Comp == P and b == "P") :
+    print("It's a tie game !")
+elif (Comp == Ss and b == "Ss") :
+    print("It's a tie game !")
+elif (Comp == P and b == "S") :
+
+    print("Sorry, You have lost the game !")
+elif (Comp == Ss and b == "P") :
+    print("Sorry, You have lost the game !")
+elif (Comp == S and b == "Ss") :
+    print("Sorry, You have lost the game !")
+elif (Comp == P and b == "Ss") :
+
+    print("Congarulatios, You have won !")
+elif (Comp == Ss and b == "S") :
+    print("Congarulations, You have won !")
+elif (Comp == P and b == "S") :
+    print("Congarulatios, You have won !")
+
+print("Thankyou For Playing !")
